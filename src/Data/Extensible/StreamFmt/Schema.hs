e65ce999f4f24ad0bf6ec762275aca472c0ddf12
Alex Palmer <alex.palmer643@gmail.com>
2022-06-11 15:06:04 +1200

    exposed schemaOf

--- src/Data/Extensible/StreamFmt/Schema.hs
+++ src/Data/Extensible/StreamFmt/Schema.hs
@@ -1,10 +1,12 @@
 module Data.Extensible.StreamFmt.Schema
   ( Schema
   , schema
+  , schemaOf
   ) where
 
 import           Data.Extensible.StreamFmt.Schema.Internal.Extensible
 import           Data.Extensible.StreamFmt.Schema.Internal.Primitives
 import           Data.Extensible.StreamFmt.Schema.Internal.Serialize
 import           Data.Extensible.StreamFmt.Schema.Internal.Types.Schema (Schema,
-                                                                         schema)
+                                                                         schema,
+                                                                         schemaOf)
